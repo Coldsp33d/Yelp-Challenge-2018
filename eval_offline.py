6ef3d88249dc553c3ee5bef0055b16e08f1d132c
Shiva Deviah <shivakdeviah@gmail.com>
2018-12-01 21:37:39 -0800

    Make random attribute dropout optional

--- eval_offline.py
+++ eval_offline.py
@@ -1,69 +1,90 @@
 from transform_business_preferences import (
-	NotEnoughDataException, CAT_LABEL, HOW, cols, model, suggest_business_attributes)
+	NotEnoughDataException, CAT_LABEL, HOW, cols, suggest_business_attributes)
+
+from utils import str2bool
+
 import pandas as pd
 import numpy as np
 
+import sys
 import os 
 import joblib
 import random
+import argparse 
 
-# Sample a few records
-# Randomly drop features from these records
-# Predict new, if it is higher, discard
-# Run through phase 2, update and predict again
-# Report all those with increased suggestions
-
-# SEED = 0, ACCURACY = 79.25
-# SEED = 42,
-# SEED = 1337,
+# Program flow:
+# 	1. Sample a few records
+# 	2. Randomly drop features from these records
+# 	3. Predict new, if it is higher, discard
+# 	4. Run through phase 2, update and predict again
+# 	5. Report all those with increased suggestions
 
 def transform_df(df, fdict):
     return df.drop(fdict.keys(), axis=1).join(df.apply(fdict))
 
 
-SAMPLE_SIZE = 1000
-df_ = (pd.read_csv('yelp_dataset/businesses_withCluster_{}.csv'.format(HOW))
-             .drop(['business_id', 'categories', 'rating'], 1)
-             .dropna(subset=[CAT_LABEL])
-             .sample(n=SAMPLE_SIZE * 3, seed=42))
+if __name__ == '__main__':
+	SAMPLE_SIZE = int(sys.argv[1])
+	SEED = int(sys.argv[2]) if len(sys.argv) > 2 else 0
 
-preds1 = model.clf.predict(transform_df(df_, model.fdict).reindex(cols, axis=1).values)
+	parser = argparse.ArgumentParser(description='Business Success Prediction - Offline Evaluation Script.')
+	parser.add_argument('--randomDropout', 
+	                    required=False,
+	                    type=str2bool,
+	                    nargs='?',
+	                    const=True, 
+	                    default=False,
+	                    help="Randomly drop attributes in samples before suggestion.")
 
-records = df_.agg(lambda x: x.dropna().to_dict(), axis=1).tolist()
+	args = vars(parser.parse_args())
 
-# Randomly remove anywhere between 2 and 4 non-null attributes.
-random.seed(0)
-for r in records:
-    r.update(dict.fromkeys(random.choices(list(r.keys() - {CAT_LABEL}), k=random.randint(2, 4)), np.nan))    
+	df_ = (pd.read_csv('yelp_dataset/businesses_withOtherBusinesses_withCluster_{}_test.csv'.format(HOW))
+	             .drop(['business_id', 'categories', 'rating'], 1)
+	             .dropna(subset=[CAT_LABEL])
+	             .sample(n=SAMPLE_SIZE, random_state=SEED))
 
-dropped_df = pd.DataFrame(records)
-dropped_df = dropped_df.pipe(transform_df, fdict=model.fdict).reindex(cols, axis=1)
+	model = model = joblib.load('models/model_withCluster_{}_train'.format(HOW))
+	preds1 = model.clf.predict(transform_df(df_, model.fdict).reindex(cols, axis=1).values)
 
-preds2 = model.clf.predict(dropped_df.values)
-print('Businesses with improved ratings after feature dropout: {} (out of {})'.format((preds2 >= preds1).sum(), len(preds2)))
+	records = df_.agg(lambda x: x.dropna().to_dict(), axis=1).tolist()
 
-# Drop every invalid row and take the first X number of valid businesses.
-idx = np.random.choice(np.arange(len(df_))[preds2 < preds1], SAMPLE_SIZE, replace=False)
-df_ = df_.iloc[idx]
-preds1 = preds1[idx]
-preds2 = preds2[idx]
+	if args['randomDropout']:
+		# Randomly remove anywhere between 2 and 4 non-null attributes
+		random.seed(SEED)
+		for r in records:
+		    r.update(dict.fromkeys(random.choices(list(r.keys() - {CAT_LABEL}), k=random.randint(2, 4)), np.nan))    
 
-records_filtered_suggested_attributes = {}
-for i, (r, p1, p2) in enumerate(zip(records, preds1, preds2)):
-	if p1 > p2:
-		try:
-			records_filtered_suggested_attributes[i] = suggest_business_attributes(r, r[CAT_LABEL], p2)
-		except NotEnoughDataException:
-			pass
+		dropped_df = pd.DataFrame(records)
+		dropped_df = dropped_df.pipe(transform_df, fdict=model.fdict).reindex(cols, axis=1)
 
-suggested_df = (transform_df(pd.DataFrame.from_dict(records_filtered_suggested_attributes, orient='index'), model.fdict)
-					.reindex(index=pd.np.arange(df_.shape[0]), columns=cols))
-preds3 = model.clf.predict(suggested_df.values.tolist())
+		preds2 = model.clf.predict(dropped_df.values)
+		print('Businesses with improved ratings after feature dropout: {} (out of {})'
+				.format((preds2 >= preds1).sum(), len(preds2)))
 
-m = suggested_df.isna().all(1)
-preds1, preds2, preds3 = preds1[~m], preds2[~m], preds3[~m]
-diff = preds3 - preds2
+		# Drop every invalid row and take the first X number of valid businesses.
+		idx = np.random.choice(np.arange(len(df_))[preds2 < preds1], SAMPLE_SIZE, replace=False)
+		df_ = df_.iloc[idx]
+		preds1 = preds1[idx]
+		preds2 = preds2[idx]
+	else:
+		print("Size of testing sample:", len(df_))
+		preds2 = preds1
 
+	records_filtered_suggested_attributes = {}
+	for i, (r, p1, p2) in enumerate(zip(records, preds1, preds2)):
+		if p1 >= p2:
+			try:
+				records_filtered_suggested_attributes[i] = suggest_business_attributes(r, r[CAT_LABEL], p2)
+			except NotEnoughDataException:
+				pass
 
-print("Accuracy: {}%".format((preds3 > preds2).mean() * 100))
-print("Average Improvement:", (np.sign(diff) * (diff ** 2)).mean() ** .5)
+	suggested_df = (transform_df(pd.DataFrame.from_dict(records_filtered_suggested_attributes, orient='index'), model.fdict)
+						.reindex(index=pd.np.arange(df_.shape[0]), columns=cols))
+	preds3 = model.clf.predict(suggested_df.values.tolist())
+
+	m = suggested_df.isna().all(1)
+	preds1, preds2, preds3 = preds1[~m], preds2[~m], preds3[~m]
+	diff = preds3 - preds2
+
+	print("Accuracy: {}%".format((preds3 > preds2).mean() * 100))
+	print("Average Improvement (+ve examples):", diff[diff > 0].mean() ** .5)
--- transform_business_preferences.py
+++ transform_business_preferences.py
@@ -1,8 +1,13 @@
+from sklearn.preprocessing import MinMaxScaler
+
 import copy
 import pandas as pd
 import joblib
 from utils import NlpLite
 import os
+
+class NotEnoughDataException(Exception):
+    pass
 
 CAT_LABEL, HOW = [('catRegion_label', 'Regional'), ('cat152_label', 'Svd')][1]  # Change to [0] for Regional
 
@@ -11,6 +16,7 @@ nlp = NlpLite(os.path.join(os.path.dirname(__file__), 'models/nlplite'))
 df = (pd.read_csv(os.path.join(os.path.dirname(__file__), 'yelp_dataset/businesses_withCluster_{}.csv'.format(HOW)))
         .drop('business_id', 1)
         .dropna(subset=[CAT_LABEL]))
+df['rating'] = MinMaxScaler((1, 10)).fit_transform(df['rating'].values[:, None])
 		
 model = joblib.load(os.path.join(os.path.dirname(__file__), 'models/model_withCluster_{}'.format(HOW)))
 
@@ -22,6 +28,27 @@ cols = (pd.read_csv(os.path.join(os.path.dirname(__file__), 'yelp_dataset/busine
           .drop(['business_id', 'categories', 'rating'], axis=1)
           .columns
           .tolist())
+
+# Attributes to suggest improvements on.
+suggested_attributes_list = [
+    'Alcohol',
+    'BikeParking',
+    'BusinessAcceptsCreditCards',
+    'Caters',
+    'GoodForKids',
+    'HasTV',
+    'NoiseLevel',
+    'OutdoorSeating',
+    'RestaurantsAttire',
+    'RestaurantsDelivery',
+    'RestaurantsGoodForGroups',
+    'RestaurantsPriceRange2',
+    'RestaurantsReservations',
+    'RestaurantsTableService',
+    'RestaurantsTakeOut',
+    'WheelchairAccessible',
+    'WiFi'
+]
 
 def preprocess_user_data(business_preferences, business_category):
     business_preferences = copy.deepcopy(business_preferences)
@@ -43,3 +70,44 @@ def preprocess_user_data(business_preferences, business_category):
     return (
         most_similar_business_category, 
         new.drop(model.fdict.keys(), 1).join(new.apply(model.fdict)).reindex(cols, axis=1).T.squeeze())
+
+
+def suggest_business_attributes(business_preferences, business_category_label, business_predicted_rating):
+    m = (df[CAT_LABEL].eq(business_category_label) 
+     & df['RestaurantsPriceRange2']
+            .sub(business_preferences.get('RestaurantsPriceRange2', 2.5))
+            .abs()
+            .lt(2))
+    all_businesses_in_category = df[m]
+
+    # consider only ratings that are greater than the predicted business rating
+    all_better_businesses_in_category = all_businesses_in_category[
+        all_businesses_in_category['rating'] > business_predicted_rating]
+
+    # Take top 10 percent of the ratings, or 20 businesses, whichever is higher.
+    k = max(20, int(len(all_better_businesses_in_category) * 0.1)) 
+
+    best_k = (all_better_businesses_in_category
+                .sort_values('rating', ascending=False).reset_index(drop=True))
+
+    if not len(best_k):
+        raise NotEnoughDataException("Not enough data for prediction.")
+
+    best_output = {}
+    for attribute in suggested_attributes_list:
+        t = best_k.dropna(subset=[attribute])
+
+        if t.empty:
+            best_output[attribute] = pd.np.nan
+        else:
+            best_output[attribute] = (
+                t.assign(rating=t.rating * 1/pd.np.arange(1, len(t)+1))
+                  .groupby(attribute).rating
+                  .sum()
+                  .sort_values(ascending=False)
+                  .index[0])
+
+    diff = business_preferences.copy()
+    diff.update(best_output)
+
+    return diff
